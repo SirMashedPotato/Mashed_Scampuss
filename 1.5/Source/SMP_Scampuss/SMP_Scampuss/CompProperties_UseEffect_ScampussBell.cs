9544dfb1779a94dba23f0a4011563fa36d171daf
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-03-20 17:22:38 +1300

    Update to 1.0.7 - 1.5 Support

---
+++ 1.5/Source/SMP_Scampuss/SMP_Scampuss/CompProperties_UseEffect_ScampussBell.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+using RimWorld;
+
+namespace SMP_Scampuss
+{
+    class CompProperties_UseEffect_ScampussBell : CompProperties_UseEffect
+    {
+        public CompProperties_UseEffect_ScampussBell()
+        {
+            compClass = typeof(Comp_UseEffect_ScampussBell);
+        }
+
+        public PawnKindDef pawnDef = null;
+        public SoundDef soundDef = null;
+    }
+}
---
+++ 1.5/Source/SMP_Scampuss/SMP_Scampuss/Comp_UseEffect_ScampussBell.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+using RimWorld;
+using Verse.AI;
+using Verse.Sound;
+
+namespace SMP_Scampuss
+{
+    class Comp_UseEffect_ScampussBell : CompUseEffect
+    {
+		public CompProperties_UseEffect_ScampussBell Props
+		{
+			get
+			{
+				return (CompProperties_UseEffect_ScampussBell)props;
+			}
+		}
+
+        public override void DoEffect(Pawn usedBy)
+        {
+            base.DoEffect(usedBy);
+            if (Props.soundDef != null)
+            {
+                Props.soundDef.PlayOneShot(new TargetInfo(usedBy.Position, usedBy.Map, false));
+            }
+            if (Props.pawnDef != null)
+            {
+                Pawn pawn = PawnGenerator.GeneratePawn(Props.pawnDef, usedBy.Faction);
+                PawnUtility.TrySpawnHatchedOrBornPawn(pawn, usedBy);
+
+                pawn.jobs.StartJob(new Job(JobDefOf.Nuzzle, usedBy));
+                Messages.Message("SMP_ScampussAppears".Translate(usedBy.Name), pawn, MessageTypeDefOf.PositiveEvent, true);
+
+            }
+        }
+    }
+}
